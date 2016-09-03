e6a3bbec9f20e4d0da19933c415fd3b7303ba1d8
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2016-09-03 22:33:32 +0800

    Adding tab generator function

--- balance.py
+++ balance.py
@@ -38,6 +38,12 @@ class Row(namedtuple('Row', ('value', 'date', 'comment'))):
     def __radd__(self, value):
         return self.__add__(value)
 
+def find_hashtag(keyword, rows):
+   '''Find a hash tag in the payment history'''
+   for row in rows:
+      if(row.comment.__contains__('#'+keyword)):
+         return True,-row.value,row.date
+   return False,'$0','Not yet'
 
 def parse_dir(dirname):
     '''Take all files in dirname and return Row instances'''
@@ -51,6 +57,22 @@ def parse_dir(dirname):
             for row in reader:
                 yield Row(*row, direction=direction)
 
+def parse_outgoing_payments(dirname,date):
+    '''Take all files in dirname and return Row instances'''
+
+    with open(os.path.join(dirname, 'outgoing-'+date), 'r') as tsvfile:
+        reader = csv.reader(tsvfile, delimiter='\t')
+
+        for row in reader:
+            yield Row(*row, direction='outgoing')
+
+def get_outgoing_payment_months(dirname):
+    ret_array=[]
+    for filename in os.listdir(dirname):
+        direction, date = filename.split('-', 1)
+        if direction == "outgoing":
+             ret_array.append(date)
+    return ret_array
 
 argparser = argparse.ArgumentParser(
     description='Run calculations and transformations on cash data')
@@ -61,6 +83,8 @@ argparser.add_argument('--dir',
                        help='Input directory')
 subp = argparser.add_subparsers(help='Subcommand', dest='cmd')
 subp.required = True
+subp.add_parser('topay', help='List all pending payments')
+subp.add_parser('topay_html', help='List all pending payments as HTML table')
 subp.add_parser('sum', help='Sum all transactions')
 subp.add_parser('party', help='Is it party time or not?')
 csv_parser = subp.add_parser('csv', help='Output transactions as csv')
@@ -80,6 +104,36 @@ if __name__ == '__main__':
 
     if args.cmd == 'sum':
         print("{}".format(sum(parse_dir(args.dir))))
+
+    elif args.cmd == 'topay':
+        for date in get_outgoing_payment_months(args.dir):
+            print('Date: '+date)
+            print('Bill       \tPrice\tPay Date')
+            rows = sorted(parse_outgoing_payments(args.dir,date), key=lambda x: x.date)
+            paid,price,date = find_hashtag('rent',rows)
+            print('Rent       \t'+str(price)+'\t'+str(date))
+            paid,price,date = find_hashtag('electricity',rows)
+            print('Electricity\t'+str(price)+'\t'+str(date))
+            paid,price,date = find_hashtag('internet',rows)
+            print('Internet   \t'+str(price)+'\t'+str(date))
+            paid,price,date = find_hashtag('water',rows)
+            print('Water      \t'+str(price)+'\t'+str(date))
+
+    elif args.cmd == 'topay_html':
+        for date in get_outgoing_payment_months(args.dir):
+            print('<h2>Date: <i>'+date+'</i></h2>')
+            rows = sorted(parse_outgoing_payments(args.dir,date), key=lambda x: x.date)
+            print('<table>')
+            print('<tr><th>Bills</th><th>Price</th><th>Pay Date</th></tr>')
+	    paid,price,date = find_hashtag('rent',rows)
+            print('<tr><td>Rent</td><td>'+str(price)+'</td><td>'+str(date)+'</td></tr>')
+	    paid,price,date = find_hashtag('electricity',rows)
+            print('<tr><td>Electric</td><td>'+str(price)+'</td><td>'+str(date)+'</td></tr>')
+            paid,price,date = find_hashtag('internet',rows)
+            print('<tr><td>Internet</td><td>'+str(price)+'</td><td>'+str(date)+'</td></tr>')
+            paid,price,date = find_hashtag('water',rows)
+            print('<tr><td>Water</td><td>'+str(price)+'</td><td>'+str(date)+'</td></tr>')
+            print('</table>')
 
     elif args.cmd == 'party':
         balance = sum(parse_dir(args.dir))
