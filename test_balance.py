f1cea4273a7f570414a0041bb8af64e54380dd54
Hamish Coleman <hamish@zot.org>
2016-10-07 10:42:04 +0800

    Add a generic filtering feature and use it where simple filtering is done

--- test_balance.py
+++ test_balance.py
@@ -18,10 +18,12 @@ class TestRowClass(unittest.TestCase):
         self.assertEqual(object.value,100)
         self.assertEqual(object.comment,"a comment")
         self.assertEqual(object.date,datetime.datetime(1970, 1, 1, 0, 0))
+        self.assertEqual(object.direction,'incoming')
 
     def test_outgoing(self):
         object = balance.Row("100","1970-01-01","a comment","outgoing")
         self.assertEqual(object.value,-100)
+        self.assertEqual(object.direction,'outgoing')
 
     def test_addnum(self):
         object = balance.Row("100","1970-01-01","a comment","incoming")
@@ -36,6 +38,18 @@ class TestRowClass(unittest.TestCase):
         object2 = balance.Row("10","1971-01-01","a comment2","incoming")
         object3 = object1 + object2
         self.assertEqual(object3,110)
+
+    def test_month(self):
+        object = balance.Row("100","1970-01-01","a comment","incoming")
+        self.assertEqual(object.month(),"1970-01")
+
+    def test_match(self):
+        object = balance.Row("100","1970-01-01","a comment","incoming")
+        with self.assertRaises(AttributeError):
+            object.match(foo='blah')
+
+        self.assertEqual(object.match(direction='flubber'),None)
+        self.assertEqual(object.match(comment='a comment'),object)
 
 class TestMisc(unittest.TestCase):
 
@@ -56,4 +70,17 @@ class TestMisc(unittest.TestCase):
             (True, -10, datetime.datetime(1971, 1, 1, 0, 0))
         )
 
-        
+    def test_filter_outgoing_payments(self):
+        rows = []
+        rows.append(balance.Row("10","1970-01-05","comment1","incoming"))
+        rows.append(balance.Row("10","1970-01-10","comment2","outgoing"))
+        rows.append(balance.Row("10","1970-01-01","comment3","outgoing"))
+        rows.append(balance.Row("10","1970-02-06","comment4","outgoing"))
+
+        self.assertEqual(
+            balance.filter_outgoing_payments(rows,'1970-01'),
+            [
+                balance.Row('10','1970-01-01','comment3','outgoing'),
+                balance.Row('10','1970-01-10','comment2','outgoing'),
+            ]
+        )
