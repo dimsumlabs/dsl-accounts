798f8d65b46a34677dca0789154414ff9f66a83a
Hamish Coleman <hamish@zot.org>
2016-10-19 13:53:54 +0800

    Regularise test data now that we do not raise errors if multiple same-tagged payments are in one month

--- test_balance.py
+++ test_balance.py
@@ -69,12 +69,13 @@ class TestRowClass(unittest.TestCase):
 
 class TestMisc(unittest.TestCase):
     def setUp(self):
-        r = [None for x in range(5)]
+        r = [None for x in range(6)]
         r[0] = balance.Row("10", "1970-02-06", "comment4", "outgoing")
         r[1] = balance.Row("10", "1970-01-05", "comment1", "incoming")
         r[2] = balance.Row("10", "1970-01-10", "comment2 #rent", "outgoing")
         r[3] = balance.Row("10", "1970-01-01", "comment3 #water", "outgoing")
         r[4] = balance.Row("10", "1970-03-01", "comment5 #rent", "outgoing")
+        r[5] = balance.Row("15", "1970-01-11", "comment6 #water", "outgoing")
         self.rows = r
 
     def tearDown(self):
@@ -94,7 +95,7 @@ class TestMisc(unittest.TestCase):
 table_start:
 table_row: Out rent, -10, 1970-01-10 00:00:00
 table_row: Out unknown, $0, Not Yet
-table_row: Out water, -10, 1970-01-01 00:00:00
+table_row: Out water, -25, 1970-01-11 00:00:00
 table_end:
 header: 1970-02
 table_start:
@@ -112,9 +113,6 @@ table_end:
         )
 
     def test_grid_render(self):
-        self.rows.append(balance.Row("15", "1970-01-11",
-                                     "second #water", "outgoing"))
-
         expect = ""
         expect += "           	1970-01	1970-02	1970-03	\n"
         expect += "In unknown 	     10			\n"
