fae977cf7e8d2ade63ca991b94e2940ea19043fa
Hamish Coleman <hamish@zot.org>
2016-10-25 17:19:41 +0800

    Add a test that demonstrates the rounding and accumulated remainder process

--- test_balance.py
+++ test_balance.py
@@ -10,13 +10,14 @@ import balance
 
 class TestRowClass(unittest.TestCase):
     def setUp(self):
-        r = [None for x in range(6)]
+        r = [None for x in range(7)]
         r[0] = balance.Row("100", "1970-01-01", "incoming comment", "incoming")
         r[1] = balance.Row("100", "1970-01-02", "outgoing comment", "outgoing")
         r[2] = balance.Row( "10", "1970-01-03", "a !bangtag", "incoming") # noqa
         r[3] = balance.Row("100", "1970-01-04", "a #hashtag", "incoming")
         r[4] = balance.Row("100", "1972-02-29", "!months:-1:5", "incoming")
         r[5] = balance.Row("100", "1972-01-31", "!months:4", "incoming")
+        r[6] = balance.Row("100", "1970-01-05", "!months:3", "incoming")
         self.rows = r
 
     def tearDown(self):
@@ -131,6 +132,8 @@ class TestRowClass(unittest.TestCase):
 
     def test_autosplit(self):
         self.assertEqual(self.rows[0].autosplit(), [self.rows[0]])
+
+        # showing we can have a leap day if it is the original row date
         self.assertEqual(self.rows[4].autosplit(), [
             balance.Row("20", "1972-01-29", "!months:-1:5 !child", "incoming"),
             balance.Row("20", "1972-02-29", "!months:-1:5 !child", "incoming"),
@@ -138,11 +141,20 @@ class TestRowClass(unittest.TestCase):
             balance.Row("20", "1972-04-29", "!months:-1:5 !child", "incoming"),
             balance.Row("20", "1972-05-29", "!months:-1:5 !child", "incoming"),
         ])
+
+        # showing the end of month clamping to different values
         self.assertEqual(self.rows[5].autosplit(), [
             balance.Row("25", "1972-01-31", "!months:4 !child", "incoming"),
             balance.Row("25", "1972-02-28", "!months:4 !child", "incoming"),
             balance.Row("25", "1972-03-31", "!months:4 !child", "incoming"),
             balance.Row("25", "1972-04-30", "!months:4 !child", "incoming"),
+        ])
+
+        # showing the rounding and kept remainder
+        self.assertEqual(self.rows[6].autosplit(), [
+            balance.Row("33.34", "1970-01-05", "!months:3 !child", "incoming"),
+            balance.Row("33.33", "1970-02-05", "!months:3 !child", "incoming"),
+            balance.Row("33.33", "1970-03-05", "!months:3 !child", "incoming"),
         ])
 
     def test_match(self):
